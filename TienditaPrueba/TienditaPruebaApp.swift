ae8cb32a6632e9b4ad01b208885e57a154588747
GerardoGlobant1 <gerardo.gordillo@globant.com>
2023-02-27 22:17:47 -0600

    Initial Commit

---
+++ TienditaPrueba/TienditaPruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TienditaPruebaApp.swift
+//  TienditaPrueba
+//
+//  Created by Gerardo Gordillo Hernandez on 27/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TienditaPruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
